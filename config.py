ac2352a1e58145ec68236145d1ffae4bb72a0093
JOFLIX <joflixooko@outlook.com>
2019-08-07 18:59:41 +0300

    configured the app trail

---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:123@localhost/pitch'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLE_USE_CDN = True
+
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class ProdConfig(Config):
+    '''production class inherits the ,ain configurations
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:123@localhost/pitch'
+
+class TestConfig(Config):
+    '''
+    To Test out database relationship
+    '''
+    # SQLALCHEMY_DATABASE_URI
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
+
