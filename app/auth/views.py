5f6373fed98a67180dcd0ac7581868e020d00452
JOFLIX <joflixooko@outlook.com>
2019-08-08 05:48:37 +0300

    updated the auth folder

--- app/auth/views.py
+++ app/auth/views.py
@@ -45,3 +45,23 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('main.index'))# redirects user to the main page of the app after successful logout
+
+
+@auth.route("/profile", methods=['GET', 'POST'])
+@login_required
+def profile():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('auth.account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
